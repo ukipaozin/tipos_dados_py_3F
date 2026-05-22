bd401ce2acf6ddef00d357b47a854e8127d21ee4
Marcos Júnior <168776511+ukipaozin@users.noreply.github.com>
2026-05-22 09:16:09 -0300

    Add basic data types and collections in dados.py
    sadad

---
+++ dados.py
@@ -0,0 +1,30 @@
+#revisão de variaveis e tipos de dados
+#típos basicos
+
+#int: numeros inteiros(sem casas decimais)
+idade = 17
+
+#float: ponto flutuante(com casas decimais)
+altura = 1.90
+
+#string: (ou str) texto puro
+nome = "Tarturtles"
+
+#bool: booleano (sim, não, verdade, mentira)
+Gerar = True
+
+#tipos de coleção
+#lista coleção ordenada
+frutas = ["maça", "banana", "uva", "pera"]
+
+#tupla: geralmente usada para guardar coordenadas
+cianorte = (-23.6631, -52.6054)
+
+#set: conjunto de dados não repitidos
+numeros ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+
+#dic: dicionario
+pessoa = {"nome": "Tarturtles", "idade": 17, "altura": 1.80}
+
+#None type, quando algo precisa ser nulo
+valor = None
